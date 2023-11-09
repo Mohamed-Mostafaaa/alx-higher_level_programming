32dd577075fb6b4891fd7e09f608f621bcc7a9df
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2023-11-09 12:56:36 +0200

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 
 
 def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+    if not a_dictionary or len(a_dictionary) == 0:
         return None
 
     ret = list(a_dictionary.keys())[0]
@@ -11,4 +11,4 @@ def best_score(a_dictionary):
         if v > big:
             big = v
             ret = k
-    return (ret)
+    return ret
