7f0be03e9e0a0561c6eb1a86a578c1f1b631c55f
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2024-01-19 18:10:40 +0200

    Only Comedy

--- 0X0E-SQL_more_queries/15-comedy_only.sql
+++ 0X0E-SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,6 @@
 -- Script that lists genres of Dexter show
-SELECT tv_shows.title -- Query to join tables
+
+SELECT tv_shows.title 
 FROM tv_shows
      JOIN tv_show_genres
      	  ON tv_show_genres.show_id = tv_shows.id
