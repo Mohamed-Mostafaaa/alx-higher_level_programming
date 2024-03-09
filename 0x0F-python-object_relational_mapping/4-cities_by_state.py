e3bd00ae76df33c2087d97ddcaf029ee42775897
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2024-03-09 15:20:52 +0200

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,39 +1,24 @@
 #!/usr/bin/python3
-"""Display cities"""
+"""
+Script that lists all cities from the database hbtn_0e_4_usa
+"""
 import MySQLdb
 from sys import argv
 
+# The code should not be executed when imported
+if __name__ == '__main__':
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
-"""Takes arguments argv to list from database
-Only lists with states that matches name argument
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                ORDER BY cities.id ASC")
 
-Arguments:
-	argv[1]: mysql username
-	argv[2]: mysql password
-	argv[3]: database name
-"""
-db = MySQLdb.connect(host="localhost",
-						port=3306,
-						user=argv[1],
-						passwd=argv[2],
-						db=argv[3],
-						charset="utf8",
-						)
-
-# Getting a cursor in MySQLdb python
-cur = db.cursor()
-
-# Executing db queries
-cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
-			INNER JOIN states ON cities.state_id = states.id\
-			ORDER BY cities.id ASC")
-
-# fetches all the rows of a query result
-query_rows = cur.fetchall()
-
-# Printing the result one in one
-for row in query_rows:
-	print(row)
-
-cur.close()
-db.close()
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    # Clean up process
+    cur.close()
+    db.close()
