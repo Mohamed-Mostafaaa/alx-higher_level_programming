e7c0d5c1d580ac9166e7e10d0e6a06abeac8058d
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2024-03-09 15:19:34 +0200

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,37 +1,27 @@
 #!/usr/bin/python3
-"""Display name argument of states table"""
+"""
+Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument
+"""
 import MySQLdb
 from sys import argv
 
+# The code should not be executed when imported
+if __name__ == '__main__':
 
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
-"""Takes arguments argv to list from database
-Only lists with states that start with  N
-	argv[1]: mysql username
-	argv[2]: mysql password
-	argv[3]: database name
-"""
-db = MySQLdb.connect(host="localhost",
-						port=3306,
-						user=argv[1],
-						passwd=argv[2],
-						db=argv[3],
-						charset="utf8",
-						)
+    # It gives us the ability to have multiple seperate working environments
+    # through the same connection to the database.
+    cur = db.cursor()
+    nmeSr = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4])
+    cur.execute(nmeSr)
 
-# Getting a cursor in MySQLdb python
-cur = db.cursor()
-
-# Executing db queries
-cur.execute("SELECT * FROM states WHERE BINARY name='{:s}'\
-			ORDER BY id ASC".format(argv[4]))
-
-# fetches all the rows of a query result
-query_rows = cur.fetchall()
-
-# Printing the result one in one
-for row in query_rows:
-	print(row)
-
-cur.close()
-db.close()
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    # Clean up process
+    cur.close()
+    db.close()
